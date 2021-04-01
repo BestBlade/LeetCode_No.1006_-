6f1e251bf079ba7289efb8872805d1cb2e102539
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-04-01 14:58:51 +0800

    Add files via upload

---
+++ leetcode_no1006_笨阶乘_w1.cpp
@@ -0,0 +1,114 @@
+﻿/* ------------------------------------------------------------------|
+通常，正整数 n 的阶乘是所有小于或等于 n 的正整数的乘积。例如，factorial(10) = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1。
+
+相反，我们设计了一个笨阶乘 clumsy：在整数的递减序列中，我们以一个固定顺序的操作符序列来依次替换原有的乘法操作符：乘法(*)，除法(/)，加法(+)和减法(-)。
+
+例如，clumsy(10) = 10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1。然而，这些运算仍然使用通常的算术运算顺序：我们在任何加、减步骤之前执行所有的乘法和除法步骤，并且按从左到右处理乘法和除法步骤。
+
+另外，我们使用的除法是地板除法（floor division），所以 10 * 9 / 8 等于 11。这保证结果是一个整数。
+
+实现上面定义的笨函数：给定一个整数 N，它返回 N 的笨阶乘。
+
+ 
+
+示例 1：
+
+输入：4
+输出：7
+解释：7 = 4 * 3 / 2 + 1
+示例 2：
+
+输入：10
+输出：12
+解释：12 = 10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1
+ 
+
+提示：
+
+1 <= N <= 10000
+-2^31 <= answer <= 2^31 - 1  （答案保证符合 32 位整数。）
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/clumsy-factorial
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	自动状态机
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：5.8 MB, 在所有 C++ 提交中击败了61.21%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+int clumsy(int N) {
+    int res = 0;
+    int flag = -1;
+    int status = 0;
+    int tmp = 0;
+    for (int i = N; i > 0; --i) {
+        if (status == 0) {
+            tmp += i;
+            status++;
+        }
+        else if (status == 1) {
+            tmp *= i;
+            status++;
+        }
+        else if (status == 2) {
+            tmp /= i;
+            status++;
+        }
+        else if (status == 3) {
+            tmp += flag * (-1) * i;
+            status++;
+        }
+
+        if (status == 4) {
+            res += flag * (-1) * tmp;
+            status = 0;
+            tmp = 0;
+            flag = 1;
+        }
+    }
+    res += flag * (-1) * tmp;
+    return res;
+}
